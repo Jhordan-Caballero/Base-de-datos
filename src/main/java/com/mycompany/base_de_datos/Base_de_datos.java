874d3a0284bfe4b91babac6829a0b88b41a9f3c9
Jhordan <jcaballerooros@gmail.com>
2025-08-17 18:24:37 -0500

    ahora si se hace una correcta subida de archivos

---
+++ src/main/java/com/mycompany/base_de_datos/Base_de_datos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.base_de_datos;
+
+/**
+ *
+ * @author jcaba
+ */
+public class Base_de_datos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
